1f15f5c80cc074b0ab840ab350b7537fab47ac2c
alserene <aserene@swin.edu.au>
2025-05-15 16:00:04 +1000

    Align applet placeholder frame with design.

--- src/components/applet.jsx
+++ src/components/applet.jsx
@@ -2,12 +2,12 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 function Applet() {
   return (
-    <Card className="rounded-md border-none pt-0 pb-0 bg-gray-800 gap-3">
-      <CardHeader className="bg-teal-900 py-3 rounded-xs border-teal-900 ">
-        <CardTitle className="text-white font-light">Content Header</CardTitle>
+    <Card className="border-none p-0 bg-stone-800 gap-2">
+      <CardHeader className="grid-rows-1 bg-teal-900 p-4 rounded-sm align-center">
+        <CardTitle className="text-white font-thin">Content Header</CardTitle>
       </CardHeader>
-      <CardContent className="grid gap-4 bg-black py-2 text-neutral-200 rounded-sm border-teal-900 h-80">
-        <div> Content goes here!</div>
+      <CardContent className="grid gap-4 bg-black p-4 text-neutral-200 rounded-sm border-2 border-teal-900 h-80 font-thin">
+        <div> Content goes here. </div>
       </CardContent>
     </Card>
   );
