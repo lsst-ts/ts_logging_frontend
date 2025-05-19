f8a31bc8b9e88392731b7261fa63d6fd58721e7b
Eman Ali <eali@swin.edu.au>
2025-05-19 18:51:54 +1000

    add doc string for calculate efficiency

--- src/utils/fetchUtils.jsx
+++ src/utils/fetchUtils.jsx
@@ -1,3 +1,11 @@
+/**
+ * Calculates the efficiency of night hours usage, accounting for exposure time and weather loss.
+ *
+ * @param {number} nightHours - The total number of night hours available.
+ * @param {number} sumExpTime - The sum of exposure time in seconds.
+ * @param {number} weatherLoss - The total time lost due to weather in seconds.
+ * @returns {number} The calculated efficiency as a ratio (0 if nightHours is 0).
+ */
 const calculateEfficiency = (nightHours, sumExpTime, weatherLoss) => {
   let eff = 0.0;
   if (nightHours !== 0) {
