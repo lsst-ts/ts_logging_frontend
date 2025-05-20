78edd37d858ae6887d5d467383dd510b13ce5216
alserene <aserene@swin.edu.au>
2025-05-20 15:50:06 +1000

    Add download and info icons.

--- src/components/metrics-card.jsx
+++ src/components/metrics-card.jsx
@@ -1,13 +1,12 @@
-// import {
-//   Tooltip,
-//   TooltipContent,
-//   TooltipProvider,
-//   TooltipTrigger,
-// } from "@/components/ui/tooltip";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
-// import InfoIcon from "../assets/InfoIcon.svg";
+import InfoIcon from "../assets/InfoIcon.svg";
 
-export default function MetricsCard({ icon, data, label, metadata }) {
+export default function MetricsCard({ icon, data, label, metadata, tooltip }) {
   return (
     <div className="flex flex-col justify-between bg-teal-900 text-white font-light p-4 rounded-lg shadow-[4px_4px_4px_0px_#0369A1]">
       <div className="flex flex-row justify-between h-12">
@@ -25,17 +24,14 @@ export default function MetricsCard({ icon, data, label, metadata }) {
           <div className="text-md">{label}</div>
           {metadata && <div className="text-sm">{metadata}</div>}
         </div>
-        {/* Code for tooltip, currently works only for mouse hover, not by touch on tablet. */}
-        {/* {tooltip &&
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger className="self-end min-w-4"><img src={InfoIcon}/></TooltipTrigger>
-              <TooltipContent>
-                <p>{tooltip}</p>
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        } */}
+        {tooltip && (
+          <Popover>
+            <PopoverTrigger className="self-end min-w-4">
+              <img src={InfoIcon} />
+            </PopoverTrigger>
+            <PopoverContent>{tooltip}</PopoverContent>
+          </Popover>
+        )}
       </div>
     </div>
   );
