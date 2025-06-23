cfb9fe7f6bd4c98febd8d434372c7ad72a4f9716
alserene <aserene@swin.edu.au>
2025-06-23 15:56:59 +1000

    Move non-fetch utils to calcUtils.

--- src/utils/fetchUtils.jsx
+++ src/utils/fetchUtils.jsx
@@ -1,44 +1,3 @@
-import { DateTime } from "luxon";
-
-/**
- * Calculates the efficiency of night hours usage, accounting for exposure time and weather loss.
- *
- * @param {number} nightHours - The total number of night hours available.
- * @param {number} sumExpTime - The sum of exposure time in seconds.
- * @param {number} weatherLoss - The total time lost due to weather in seconds.
- * @returns {number} The calculated efficiency as a ratio (0 if nightHours is 0).
- */
-const calculateEfficiency = (nightHours, sumExpTime, weatherLoss) => {
-  let eff = 0.0;
-  if (nightHours !== 0) {
-    eff = (100 * sumExpTime) / (nightHours * 60 * 60 - weatherLoss);
-  }
-  return eff === 0 ? 0 : eff.toFixed(2);
-};
-
-/**
- * Calculates the total time loss and provides a breakdown of the loss due to weather and faults.
- *
- * @param {number} weatherLoss - The amount of time lost due to weather (in seconds).
- * @param {number} faultLoss - The amount of time lost due to faults (in seconds).
- * @returns {[string, string]} A tuple where the first element is the total time loss as a string (e.g., "5 seconds"),
- * and the second element is a string detailing the percentage breakdown of weather and fault losses.
- */
-const calculateTimeLoss = (weatherLoss, faultLoss) => {
-  let loss = weatherLoss + faultLoss;
-  let timeLoss = "0 hours";
-  let timeLossDetails = "(- weather; - fault)";
-
-  if (loss > 0) {
-    let weatherPercent = (weatherLoss / loss) * 100;
-    let faultPercent = (faultLoss / loss) * 100;
-    timeLoss = `${loss.toFixed(2)} hours`;
-    timeLossDetails = `(${weatherPercent}% weather; ${faultPercent}% fault)`;
-  }
-
-  return [timeLoss, timeLossDetails];
-};
-
 const httpProtocol = window.location.protocol;
 const host = window.location.host;
 /**
@@ -208,29 +167,6 @@ const fetchExposureFlags = async (start, end, instrument, abortController) => {
 };
 
 /**
- * Formats a given JavaScript Date object into a string format 'yyyyLLdd' using luxon.
- *
- * @param {Date|null|undefined} date - The date to format. If null or undefined, returns an empty string.
- * @returns {string} The formatted date string, or an empty string if no date is provided.
- */
-const getDayobsStr = (date) => {
-  return date ? DateTime.fromJSDate(date).toFormat("yyyyLLdd") : "";
-};
-
-/**
- * Converts a date string in 'yyyyMMdd' format that is in UTC timezone
- * to a UTC DateTime object set at 12:00:00 local time.
- *
- * @param {string} dayObsStr - The date string in 'yyyyMMdd' format (e.g., '20240607').
- * @returns luxon {DateTime} The corresponding UTC DateTime object at 12:00:00.
- */
-const getDatetimeFromDayobsStr = (dayObsStr) => {
-  return DateTime.fromFormat(dayObsStr, "yyyyMMdd", {
-    zone: "UTC",
-  }).set({ hour: 12, minute: 0, second: 0 });
-};
-
-/**
  * Fetches Jira tickets from the backend API for a specified date range and instrument.
  *
  * @async
@@ -260,13 +196,9 @@ const fetchJiraTickets = async (start, end, instrument, abortController) => {
 };
 
 export {
-  calculateEfficiency,
-  calculateTimeLoss,
   fetchExposures,
   fetchAlmanac,
   fetchNarrativeLog,
   fetchExposureFlags,
-  getDayobsStr,
-  getDatetimeFromDayobsStr,
   fetchJiraTickets,
 };
