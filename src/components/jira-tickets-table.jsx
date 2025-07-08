aed21465037c0cbd17f16249d2c772ccbccea37e
Eman Ali <eali@swin.edu.au>
2025-07-08 10:57:16 +1000

    make the jira tickets table scrollable and use sticky headers. Also make the popup wider to stop columns from running into each other

--- src/components/jira-tickets-table.jsx
+++ src/components/jira-tickets-table.jsx
@@ -11,67 +11,86 @@ import LinkIcon from "../assets/LinkIcon.svg";
 
 function JiraTicketsTable({ tickets, loading = false }) {
   return (
-    <div className="overflow-x-auto">
-      <Table className="w-full">
+    <div className="w-full">
+      <Table className="w-full table-fixed">
         <TableHeader>
-          <TableRow className="[&>th]:!text-white [&>th]:text-lg">
-            <TableHead>Number</TableHead>
-            <TableHead colSpan={3}>Summary</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Created</TableHead>
-            <TableHead>Last Updated</TableHead>
-            <TableHead className="text-right">Link</TableHead>
+          <TableRow>
+            <TableHead className="sticky top-0 z-10 !text-white text-lg">
+              Number
+            </TableHead>
+            <TableHead
+              colSpan={2}
+              className="sticky top-0 z-10 !text-white text-lg"
+            >
+              Summary
+            </TableHead>
+            <TableHead className="sticky top-0 z-10 !text-white text-lg">
+              Status
+            </TableHead>
+            <TableHead className="sticky top-0 z-10 !text-white text-lg">
+              Created
+            </TableHead>
+            <TableHead className="sticky top-0 z-10 !text-white text-lg">
+              Last Updated
+            </TableHead>
+            <TableHead className="sticky top-0 z-10 !text-white text-lg text-right">
+              Link
+            </TableHead>
           </TableRow>
         </TableHeader>
-        <TableBody>
-          {loading ? (
-            <TableRow>
-              <TableCell colSpan={8} className="text-center">
-                Loading tickets...
-              </TableCell>
-            </TableRow>
-          ) : tickets.length === 0 ? (
-            <TableRow>
-              <TableCell colSpan={8} className="text-center">
-                No tickets found.
-              </TableCell>
-            </TableRow>
-          ) : (
-            tickets.map((ticket) => (
-              <TableRow key={ticket.key}>
-                <TableCell className="font-medium">
-                  <span>{ticket.key}</span>
-                  {ticket.isNew && (
-                    <span className="ml-1 bg-amber-500 text-black text-xs p-1 rounded-full">
-                      New
-                    </span>
-                  )}
-                </TableCell>
-                <TableCell colSpan={3} className="!text-wrap break-normal">
-                  {ticket.summary}
-                </TableCell>
-                <TableCell>{ticket.status}</TableCell>
-                <TableCell>{ticket.created}</TableCell>
-                <TableCell>{ticket.updated}</TableCell>
-                <TableCell className="text-right">
-                  <a
-                    href={ticket.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-blue-600 hover:underline"
-                  >
-                    <img
-                      src={LinkIcon}
-                      alt="View Ticket"
-                      className="inline-block w-6 h-6"
-                    />
-                  </a>
+      </Table>
+      <div className="max-h-96 overflow-y-auto w-full">
+        <Table className="w-full table-fixed">
+          <TableBody>
+            {loading ? (
+              <TableRow>
+                <TableCell colSpan={7} className="text-center">
+                  Loading tickets...
                 </TableCell>
               </TableRow>
-            ))
-          )}
-        </TableBody>
-      </Table>
+            ) : tickets.length === 0 ? (
+              <TableRow>
+                <TableCell colSpan={7} className="text-center">
+                  No tickets found.
+                </TableCell>
+              </TableRow>
+            ) : (
+              tickets.map((ticket) => (
+                <TableRow key={ticket.key}>
+                  <TableCell className="font-medium">
+                    <span>{ticket.key}</span>
+                    {ticket.isNew && (
+                      <span className="ml-1 bg-amber-500 text-black text-xs p-1 rounded-full">
+                        New
+                      </span>
+                    )}
+                  </TableCell>
+                  <TableCell colSpan={2} className="!text-wrap break-normal">
+                    {ticket.summary}
+                  </TableCell>
+                  <TableCell>{ticket.status}</TableCell>
+                  <TableCell>{ticket.created}</TableCell>
+                  <TableCell>{ticket.updated}</TableCell>
+                  <TableCell className="text-right">
+                    <a
+                      href={ticket.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-blue-600 hover:underline"
+                    >
+                      <img
+                        src={LinkIcon}
+                        alt="View Ticket"
+                        className="inline-block w-6 h-6"
+                      />
+                    </a>
+                  </TableCell>
+                </TableRow>
+              ))
+            )}
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 }
