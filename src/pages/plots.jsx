2fcdb7e8d709b57067a847bdbcde42d2058580d3
alserene <aserene@swin.edu.au>
2025-07-21 14:08:56 +1000

    Add plots page.

---
+++ src/pages/plots.jsx
@@ -0,0 +1,251 @@
+import { useState, useEffect } from "react";
+import { DateTime } from "luxon";
+
+import { Toaster } from "@/components/ui/sonner";
+import { toast } from "sonner";
+import { useSearch } from "@tanstack/react-router";
+
+import { Skeleton } from "@/components/ui/skeleton";
+import { TELESCOPES } from "@/components/parameters";
+
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  // Cell,
+  XAxis,
+  YAxis,
+  // Customized,
+  // ResponsiveContainer,
+} from "recharts";
+
+import { fetchDataLogEntriesFromConsDB } from "@/utils/fetchUtils";
+import { getDatetimeFromDayobsStr } from "@/utils/utils";
+
+function MetricLineChart({ title, dataKey, data }) {
+  return (
+    <ChartContainer title={title} config={{}}>
+      <LineChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{ top: 20, right: 30, left: 20, bottom: 40 }}
+      >
+        <CartesianGrid strokeDasharray="3 3" stroke="#555" />
+        <XAxis
+          dataKey="obs_start_dt"
+          type="number"
+          domain={["auto", "auto"]}
+          scale="time"
+          tickFormatter={(tick) =>
+            new Date(tick).toLocaleTimeString("en-AU", {
+              hour: "2-digit",
+              minute: "2-digit",
+            })
+          }
+          tick={{ fill: "white" }}
+          label={{
+            value: "Time (UTC)",
+            position: "bottom",
+            fill: "white",
+            dy: 25,
+          }}
+        />
+        <YAxis
+          tick={{ fill: "white" }}
+          label={{
+            value: title,
+            angle: -90,
+            position: "insideLeft",
+            fill: "white",
+            dx: -10,
+          }}
+        />
+        <ChartTooltip
+          content={(props) => (
+            <ChartTooltipContent
+              {...props}
+              formatter={(value, name, item, index, payload) => (
+                <>
+                  <span className="text-muted-foreground">{title}</span>
+                  <span className="font-mono">{value}</span>
+                  <div className="text-muted-foreground">
+                    Exposure: {payload["exposure id"]}
+                  </div>
+                </>
+              )}
+              hideLabel
+            />
+          )}
+        />
+        <Line
+          type="monotone"
+          dataKey={dataKey}
+          // stroke=""
+          stroke="#000000"
+          strokeWidth={2}
+          // dot={{ r: 1, fill: "#0C4A47", stroke: "#0C4A47" }} // sidebar teal
+          dot={{ r: 1, fill: "#3CAE3F", stroke: "#3CAE3F" }} // shadow green
+          activeDot={{ r: 6 }}
+        />
+      </LineChart>
+    </ChartContainer>
+  );
+}
+
+function Plots() {
+  // Routing and URL params
+  const { startDayobs, endDayobs, telescope } = useSearch({ from: "/plots" });
+
+  // The end dayobs is inclusive, so we add one day to the
+  // endDayobs to get the correct range for the queries
+  const queryEndDayobs = getDatetimeFromDayobsStr(endDayobs.toString())
+    .plus({ days: 1 })
+    .toFormat("yyyyMMdd");
+  const instrument = TELESCOPES[telescope];
+
+  // For display on page
+  const instrumentName = telescope;
+  const dateRangeString =
+    startDayobs === endDayobs
+      ? `on dayobs ${startDayobs}`
+      : `in dayobs range ${startDayobs}–${endDayobs}`;
+
+  // Data
+  const [dataLogEntries, setDataLogEntries] = useState([]);
+  const [dataLogLoading, setDataLogLoading] = useState(true);
+
+  useEffect(() => {
+    if (telescope === "AuxTel") {
+      return;
+    }
+
+    // To cancel previous fetch if still in progress
+    const abortController = new AbortController();
+
+    // Trigger loading skeletons
+    setDataLogLoading(true);
+
+    // Fetch data from both sources
+    fetchDataLogEntriesFromConsDB(
+      startDayobs,
+      queryEndDayobs,
+      instrument,
+      abortController,
+    )
+      .then((consDBData) => {
+        const dataLog = consDBData.data_log ?? [];
+        // console.log(dataLog);
+
+        if (dataLog.length === 0) {
+          toast.warning(
+            "No data log records found in ConsDB for the selected date range.",
+          );
+        }
+
+        // Set the merged data to state
+        setDataLogEntries(dataLog);
+        setDataLogLoading(false);
+      })
+      .catch((err) => {
+        if (!abortController.signal.aborted) {
+          const msg = err?.message || "Unknown error";
+          toast.error("Error fetching data log!", {
+            description: msg,
+            duration: Infinity,
+          });
+        }
+      })
+      .finally(() => {
+        if (!abortController.signal.aborted) {
+          setDataLogLoading(false);
+        }
+      });
+
+    // Cleanup function
+    return () => {
+      abortController.abort();
+    };
+  }, [startDayobs, queryEndDayobs, instrument]);
+
+  // Temporary display message for AuxTel queries
+  if (telescope === "AuxTel") {
+    return (
+      <div className="flex flex-col w-full p-8 gap-4">
+        <h1 className="flex flex-row gap-3 text-white text-5xl uppercase justify-center">
+          <span className="tracking-[2px] font-extralight">
+            {instrumentName}
+          </span>
+          <span className="font-extrabold">Plots</span>
+        </h1>
+        <p className="min-h-[4.5rem] text-white font-thin text-center pb-4 flex flex-col items-center justify-center gap-2">
+          AuxTel is currently not supported in this page.
+        </p>
+        <Toaster expand={true} richColors closeButton />
+      </div>
+    );
+  }
+
+  const chartData = dataLogEntries.map((entry) => ({
+    ...entry,
+    obs_start_dt: DateTime.fromISO(entry["obs start"]).toMillis(),
+  }));
+
+  return (
+    <>
+      <div className="flex flex-col w-full p-8 gap-4">
+        {/* Page title */}
+        <h1 className="flex flex-row gap-3 text-white text-5xl uppercase justify-center">
+          <span className="tracking-[2px] font-extralight">
+            {instrumentName}
+          </span>
+          <span className="font-extrabold">Plots</span>
+        </h1>
+
+        {/* Info section */}
+        <div className="min-h-[4.5rem] text-white font-thin text-center pb-4 flex flex-col items-center justify-center gap-2">
+          {dataLogLoading ? (
+            <>
+              <Skeleton className="h-5 w-3/4 max-w-xl bg-stone-700" />
+            </>
+          ) : (
+            <>
+              <p>
+                {dataLogEntries.length} exposures returned for {instrumentName}{" "}
+                {dateRangeString}.
+              </p>
+            </>
+          )}
+        </div>
+
+        {/* Plots */}
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+          <MetricLineChart
+            title="Photometric Zero Points"
+            dataKey="zero point median"
+            data={chartData}
+          />
+          <MetricLineChart
+            title="Sky Brightness"
+            dataKey="sky bg median"
+            data={chartData}
+          />
+          <MetricLineChart
+            title="DIMM Seeing"
+            dataKey="dimm seeing"
+            data={chartData}
+          />
+          <MetricLineChart title="Airmass" dataKey="airmass" data={chartData} />
+        </div>
+      </div>
+      <Toaster expand={true} richColors closeButton />
+    </>
+  );
+}
+
+export default Plots;
