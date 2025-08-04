c545f7234e9d8f28416a3e763f737e78fb10dac5
alserene <aserene@swin.edu.au>
2025-08-04 16:00:51 +1000

    Fixed bug in buttons and inputs having inconsistent rounding.

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -11,7 +11,7 @@ export function Input({ className, type, onValueChange, ...props }) {
         "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
         "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
         "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-        "w-[200px] bg-white justify-between font-normal rounded-s shadow-[4px_4px_4px_0px_#3CAE3F] focus-visible:ring-4 focus-visible:ring-green-500/50",
+        "w-[200px] bg-white justify-between font-normal shadow-[4px_4px_4px_0px_#3CAE3F] focus-visible:ring-4 focus-visible:ring-green-500/50",
         "text-black text-base pb-1",
         className,
       )}
