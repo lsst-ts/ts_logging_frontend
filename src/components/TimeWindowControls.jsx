a2856b2da7a3fd81c9d10e64c4e88e71d619bf30
alserene <aserene@swin.edu.au>
2025-08-08 05:22:10 +0000

    Simplify logic for easier debugging of timezone bugs.

--- src/components/TimeWindowControls.jsx
+++ src/components/TimeWindowControls.jsx
@@ -11,14 +11,17 @@ import {
 import { Button } from "@/components/ui/button";
 
 export default function TimeWindowControls({
-  windowStart,
-  windowEnd,
+  fullTimeRange,
   selectedTimeRange,
   setSelectedTimeRange,
   availableDayobs,
-  timelineStart,
-  timelineEnd,
 }) {
+  const [fullXMin, fullXMax] = fullTimeRange;
+  let [currentXMin, currentXMax] = selectedTimeRange;
+  if (!fullXMin || !fullXMax) {
+    return null;
+  }
+
   return (
     <div className="flex flex-row justify-between mt-4 gap-8 text-white">
       {/* Time Inputs */}
@@ -46,13 +49,15 @@ export default function TimeWindowControls({
             inputMode="numeric"
             pattern="[0-9]{2}:[0-9]{2}"
             className="max-w-[100px] bg-background appearance-none [&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-calendar-picker-indicator]:appearance-none"
-            value={DateTime.fromMillis(windowStart).toFormat("HH:mm")}
+            value={DateTime.fromMillis(currentXMin, { zone: "utc" }).toFormat(
+              "HH:mm",
+            )}
             onChange={(e) => {
               const [h, m] = e.target.value.split(":").map(Number);
-              const newStart = DateTime.fromMillis(windowStart)
+              const newStart = DateTime.fromMillis(currentXMin)
                 .set({ hour: h, minute: m })
                 .toMillis();
-              setSelectedTimeRange([newStart, windowEnd]);
+              setSelectedTimeRange([newStart, currentXMax]);
             }}
           />
           <Input
@@ -63,13 +68,15 @@ export default function TimeWindowControls({
             inputMode="numeric"
             pattern="[0-9]{2}:[0-9]{2}"
             className="max-w-[100px] bg-background appearance-none [&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-calendar-picker-indicator]:appearance-none"
-            value={DateTime.fromMillis(windowEnd).toFormat("HH:mm")}
+            value={DateTime.fromMillis(currentXMax, { zone: "utc" }).toFormat(
+              "HH:mm",
+            )}
             onChange={(e) => {
               const [h, m] = e.target.value.split(":").map(Number);
-              const newEnd = DateTime.fromMillis(windowEnd)
+              const newEnd = DateTime.fromMillis(currentXMax)
                 .set({ hour: h, minute: m })
                 .toMillis();
-              setSelectedTimeRange([windowStart, newEnd]);
+              setSelectedTimeRange([currentXMin, newEnd]);
             }}
           />
         </div>
@@ -89,15 +96,20 @@ export default function TimeWindowControls({
             End Dayobs:
           </Label>
         </div>
+
         {/* Dayobs inputs */}
         <div className="flex flex-col gap-4">
           {/* Start dayobs */}
           <Select
             id="start-dayobs-input"
             // Get dayobs from TAI window start time
-            value={DateTime.fromMillis(windowStart)
-              .minus({ hours: 12, seconds: 37 }) // TODO: Set TAI leap seconds as a utils const.
-              .toFormat("yyyyLLdd")}
+            value={(() => {
+              const toReturn = DateTime.fromMillis(currentXMin, { zone: "utc" })
+                .minus({ hours: 12, seconds: 37 })
+                .toFormat("yyyyLLdd");
+              console.log("value shown on start dayobs: ", toReturn);
+              return toReturn;
+            })()}
             onValueChange={(newDayobsStr) => {
               // Get new dayobs
               const dayobsStart = DateTime.fromFormat(
@@ -107,24 +119,40 @@ export default function TimeWindowControls({
                   zone: "utc",
                 },
               );
+              // midnight prior to dayobs (correct)
+              console.log("dayobsStart: ", dayobsStart);
+
+              // midday 21st (correct)
+              console.log("currentXMin: ", currentXMin);
+              console.log(
+                "currentXMin (UTC): ",
+                DateTime.fromMillis(currentXMin, { zone: "utc" }),
+              );
 
               // Get start time from window
-              const [h, m] = DateTime.fromMillis(windowStart, {
-                zone: "utc",
-              })
+              const [h, m] = DateTime.fromMillis(currentXMin, { zone: "utc" })
+                .minus({ seconds: 37 })
                 .toFormat("HH:mm")
                 .split(":")
                 .map(Number);
+              console.log("h, m: ", h, m);
 
               // Set new datetime
               // If in second half of dayobs -> set as next day
               const dayobsToDate =
                 h < 12 ? dayobsStart.plus({ days: 1 }) : dayobsStart;
-              const dt = dayobsToDate.set({ hour: h, minute: m });
+              // midnight prior to dayobs (correct)
+              console.log("dayobsToDate: ", dayobsToDate);
+
+              const dt = dayobsToDate
+                .set({ hour: h, minute: m, second: 37 })
+                .setZone("utc");
+              // midday 22nd (correct)
+              console.log("dt: ", dt);
 
               // Set window
               if (dt.isValid) {
-                setSelectedTimeRange([dt.toMillis(), windowEnd]);
+                setSelectedTimeRange([dt.toMillis(), currentXMax]);
               }
             }}
           >
@@ -147,7 +175,7 @@ export default function TimeWindowControls({
           <Select
             id="end-dayobs-input"
             // Get dayobs from TAI window start time
-            value={DateTime.fromMillis(windowEnd)
+            value={DateTime.fromMillis(currentXMax)
               .minus({ hours: 12, seconds: 37 })
               .toFormat("yyyyLLdd")}
             onValueChange={(newDayobsStr) => {
@@ -157,7 +185,7 @@ export default function TimeWindowControls({
               });
 
               // Get end time from window
-              const [h, m] = DateTime.fromMillis(windowEnd, {
+              const [h, m] = DateTime.fromMillis(currentXMax, {
                 zone: "utc",
               })
                 .toFormat("HH:mm")
@@ -172,7 +200,7 @@ export default function TimeWindowControls({
 
               // Set window
               if (dt.isValid) {
-                setSelectedTimeRange([windowStart, dt.toMillis()]);
+                setSelectedTimeRange([currentXMin, dt.toMillis()]);
               }
             }}
           >
@@ -198,10 +226,9 @@ export default function TimeWindowControls({
         className="bg-white text-black w-30 h-10 font-light rounded-md shadow-[4px_4px_4px_0px_#3CAE3F] 
                 flex justify-center items-center py-2 px-4 
                 hover:shadow-[6px_6px_8px_0px_#3CAE3F] hover:scale-[1.02] hover:bg-white transition-all duration-200"
-        onClick={() => setSelectedTimeRange([timelineStart, timelineEnd])}
+        onClick={() => setSelectedTimeRange([fullXMin, fullXMax])}
         disabled={
-          selectedTimeRange[0] === timelineStart &&
-          selectedTimeRange[1] === timelineEnd
+          selectedTimeRange[0] === fullXMin && selectedTimeRange[1] === fullXMax
         }
       >
         Reset Window
