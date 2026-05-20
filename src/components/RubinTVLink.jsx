ccae98c1a439aaa27f475ac5e7c47114503ab6ec
nemovc <nemovc@gmail.com>
2026-05-20 06:05:19 +0000

    Change highlight color
    Also remove dark backgrounds on links

--- src/components/RubinTVLink.jsx
+++ src/components/RubinTVLink.jsx
@@ -33,14 +33,12 @@ export default function RubinTVLink({ dayObs, seqNum, exposureName }) {
   if (!url) return null;
 
   return (
-    // Wrap in a dark background for visibility
-    // when row is highlighted.
-    <div className="bg-stone-800 p-1 rounded">
+    <div className="p-1 rounded">
       <a
         href={url}
         target="_blank"
         rel="noopener noreferrer"
-        className="text-sky-500 underline hover:text-sky-400"
+        className="text-sky-500 underline hover:text-sky-300"
       >
         {telescope === "Simonyi" ? "Post-ISR Mosaic" : "Mount Monitor"}
       </a>
