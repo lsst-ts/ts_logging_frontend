af64fbf759df0a5a46b0574ef738983de1d89381
nemovc <nemovc@gmail.com>
2026-06-25 07:12:48 +0000

    Add UTC mode flag to prepareAlmanacData, use it in ContextFeed

--- src/pages/ContextFeed.jsx
+++ src/pages/ContextFeed.jsx
@@ -204,8 +204,10 @@ function ContextFeed() {
 
     fetchAlmanac(startDayobs, queryEndDayobs, abortController)
       .then((almanac) => {
-        const { twilightValues, twilight0DegValues } =
-          prepareAlmanacData(almanac);
+        const { twilightValues, twilight0DegValues } = prepareAlmanacData(
+          almanac,
+          { utc: true },
+        );
         setTwilightValues(twilightValues);
         setTwilight0DegValues(twilight0DegValues);
       })
