c06b823d295d5c3cf981dc1733baf5d8739d2ee4
Sebastian Aranda <aranda.sebastian@gmail.com>
2026-09-01 17:59:36 -0400

    Add method to parse endpoint requests using bucket backend format.

--- src/utils/fetchUtils.js
+++ src/utils/fetchUtils.js
@@ -1,5 +1,29 @@
 const httpProtocol = window.location.protocol;
 const host = window.location.host;
+
+const INSTRUMENT_ENDPOINTS = [
+  "exposures",
+  "exposure-flags",
+  "static-visit-map",
+];
+
+function parseEndpointForStaticFile(url) {
+  const newUrlParts = [backendLocation];
+  const endpoint = url.split("?")[0].split("/").pop();
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+  newUrlParts.push("/", endpoint);
+  if (INSTRUMENT_ENDPOINTS.includes(endpoint)) {
+    newUrlParts.push("/", queryParams.get("instrument"));
+  }
+  const dayObsStart = queryParams.get("dayObsStart");
+  const dayObsEnd = queryParams.get("dayObsEnd");
+  const fileName = `${dayObsStart}-${dayObsEnd}.json`;
+  newUrlParts.push("/", fileName);
+
+  console.log(url, "->", newUrlParts.join(""));
+  return newUrlParts.join("");
+}
+
 /**
  * The base URL for the backend API endpoints.
  *
@@ -25,7 +49,7 @@ const backendLocation =
  * and should be handled by the caller.
  */
 const fetchData = async (url, abortController) => {
-  const res = await fetch(url, {
+  const res = await fetch(parseEndpointForStaticFile(url), {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
